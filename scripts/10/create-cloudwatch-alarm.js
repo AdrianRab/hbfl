e1058c1c830123d80421dbb1bfb08ddc0a05f40f
Adrian <adrian.rabowski@gmail.com>
2022-11-22 09:32:41 +0100

    sns topic, subscription and cloudwatch alarm

--- scripts/10/create-cloudwatch-alarm.js
+++ scripts/10/create-cloudwatch-alarm.js
@@ -6,9 +6,9 @@ const { sendCloudWatchCommand: sendCommand } = require('./helpers')
 
 // Declare local variables
 const alarmName = 'hamster-elb-alarm'
-const topicArn = '/* TODO: Add your SNS topic ARN */'
-const tg = '/* TODO: Add last part of Target Group ARN */'
-const lb = '/* TODO: Add last part of Load Balancer ARN */'
+const topicArn = '/* TODO: Add your SNS topic ARN SNS::Topics::ARN*/'
+const tg = '/* TODO: Add last part of Target Group ARN EC2::Load Balancing::Target Groups::<hamsterTG> last part after a colon(:)*/'
+const lb = '/* TODO: Add last part of Load Balancer ARN EC2::Load Balancing::Load Balancers::<hamsterLB> last part after a loadbalancer/ */'
 
 async function execute () {
   try {
@@ -20,7 +20,33 @@ async function execute () {
 }
 
 function createCloudWatchAlarm (alarmName, topicArn, tg, lb) {
-  // TODO: Create alarm with PutMetricAlarmCommand
+  const params = {
+    AlarmName: alarmName,
+    ComparisonOperator: 'LessThanThreshold',
+    EvaluationPeriods: 1,
+    MetricName: 'HealthyHostCound',
+    Namespace: 'AWS/ApplicationELB',
+    Period: 60,
+    Threshold: 1,
+    AlarmActions: [
+      topicArn
+    ],
+    Dimensions: [
+      {
+        Name: 'TargetGroup',
+        Value: tg
+      },
+      {
+        Name: 'LoadBalancer',
+        Value: lb
+      }
+    ],
+    Statistic: 'Average', //Other possible options here are Minimum, Maximum, Sum, and Sample Count
+    TreatMissingData: 'breaching'
+  }
+
+  const command = new PutMetricAlarmCommand(params)
+  return sendCommand(command)
 }
 
 execute()
--- scripts/10/create-sns-subscription.js
+++ scripts/10/create-sns-subscription.js
@@ -6,8 +6,8 @@ const { sendSNSCommand: sendCommand } = require('./helpers')
 
 // Declare local variables
 const type = 'sms'
-const endpoint = '/* TODO: Add your mobile number with country code */'
-const topicArn = '/* TODO: Add your sns topic arn */'
+const endpoint = '/* TODO: Add your mobile number with country code e.g. 14255555555*/'
+const topicArn = '/* TODO: Add your sns topic arn (was returned during topic creation)*/'
 
 async function execute () {
   try {
@@ -19,7 +19,13 @@ async function execute () {
 }
 
 function createSubscription (type, topicArn, endpoint) {
-  // TODO: Subscribe to topic
+  const params = {
+    Protocol: type,
+    TopicArn: topicArn,
+    Endpoint: endpoint
+  }
+  const command = new SubscribeCommand(params)
+  return sendCommand(command)
 }
 
 execute()
